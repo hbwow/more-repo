6818d78b143a856a908c3612bc2d203c84f72523
冷开俊 <lengkj@travelsky.com.cn>
2025-01-08 11:20:41 +0800

    feat: 🎸 web-vite内置@tanstack/react-query

--- apps/web-vite/src/main.tsx
+++ apps/web-vite/src/main.tsx
@@ -3,19 +3,35 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ConfigProvider } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // 窗口聚焦重新获取
+      retry: false, // 查询重试
+      staleTime: 0,
+      cacheTime: 0,
+      refetchOnMount: 'always', // true
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        token: {
-          // Seed Token，影响范围大
-          colorPrimary: '#00b96b',
-          colorInfo: '#00b96b',
-        },
-      }}
-    >
-      <App />
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider
+        theme={{
+          token: {
+            // Seed Token，影响范围大
+            colorPrimary: '#00b96b',
+            colorInfo: '#00b96b',
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
